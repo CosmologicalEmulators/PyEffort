bc990973157f85240b53983ebeb4a881fdddb346
Marco Bonici <bonici.marco@gmail.com>
2023-03-03 02:27:26 +0100

    Added working notebook

--- src/PyEffort.py
+++ src/PyEffort.py
@@ -1,4 +1,5 @@
 from juliacall import Main as jl
+import numpy as np
 
 jl.seval("using Effort")
 jl.seval("using SimpleChains")
@@ -11,11 +12,11 @@ load_emu_jl = jl.seval('BSON.load')
 
 def compute_Pl(cosmo, bs, emu):
     Pl = effort_compute_Pl(jl.collect(cosmo), jl.collect(bs), emu)
-    return Pl
+    return np.array(Pl)
 
 def compute_Xil(cosmo, bs, emu):
-    Pl = effort_compute_Xil(jl.collect(cosmo), jl.collect(bs), emu)
-    return Pl
+    Xil = effort_compute_Xil(jl.collect(cosmo), jl.collect(bs), emu)
+    return np.array(Xil)
 
 def load_emu(path):
     loaded = load_emu_jl(path)
